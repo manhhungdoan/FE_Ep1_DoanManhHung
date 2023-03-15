4f87caf59bd7f63baf5f8d6df85394da6047753b
mhung <hungthiendoan@gmail.com>
2023-03-16 03:40:11 +0700

    add animation - finished project

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
 $(document).ready(function () {
-    $(".nav-mobile").click(function () {
-        $(".nav").toggleClass("show-menu")
+    $(".nav-mobile, .nav-item, .btn-login, .btn-startnow").click(function () {
+        $(".nav, .user-btn").toggleClass("show-menu show-btn")
+    });
+
+    $(".logo").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 1000);
+    });
+    $(".nav-features").click(function () {
+        var offset = -45; //Offset of -10px
+        $('html, body').animate({
+            scrollTop: $("#features").offset().top + offset
+        }, 1000);
+    });
+
+    $(".btn-startnow, .btn-trial").hover(function () {
+        $(this).css("background-color", "#365ca2");
+    }, function () {
+        $(this).css("background-color", "#3C64B1");
+    });
+    $(".btn-login").hover(function () {
+        $(this).css("border", "1px solid #3C64B1");
+        $(this).css("background-color", "#d1deff");
+    }, function () {
+        $(this).css("border", "1px solid #EFF3FF");
+        $(this).css("background", "none");
+    });
+    $(".btn-more").hover(function () {
+        $(this).css("background-color", "#d1deff");
+    }, function () {
+        $(this).css("background", "none");
+    });
+    $("img").hover(function () {
+        $(this).css("transform", "scale(1.125)");
+    }, function () {
+        $(this).css("transform", "scale(1)");
     });
 });
